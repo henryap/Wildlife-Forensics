638cc471252e08714d40d7550cde989eaac808d8
henryap <henry.sanchez@apollidon.com>
2016-11-03 10:39:57 -0400

    initial commit

---
+++ UFandShands/apollo/custom_footer.php
@@ -0,0 +1,22 @@
+<!-- apollo footer -->
+<section class="apollo-footer">
+	<div class="container">
+		
+		 	<!--
+		 	Footer Widget one
+		 	Edit in Wordpress Widget area
+		 	-->
+			<?php footer_widget_one() ?>
+			<!--
+			Footer Widget two
+			Edit in Wordpress Widget area
+			-->
+			<?php footer_widget_two() ?>
+			<!--
+			Footer Widget three
+			Edit in Wordpress Widget area
+			-->
+			<?php footer_widget_three() ?> 
+	</div>	
+</section>
+<!-- eof apollo footer -->
---
+++ UFandShands/apollo/ty_custom_banner.php
@@ -0,0 +1,10 @@
+<!-- apollo banner -->
+<section class="main-banner">
+	<div class="container apply-container">
+		<div class="apply-main-banner-meta">
+			<?php ufandshands_content_title(); //page title ?>
+		</div>
+		 	
+	</div>
+</section>	
+<!-- eof apollo banner -->
